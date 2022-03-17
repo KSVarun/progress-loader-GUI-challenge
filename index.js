6ca170b0101c18169e5bf7ea56ff247ac3fbfca3
KSVarun <ksvarun001@gmail.com>
2022-03-17 23:11:41 +0530

    added loading

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const incBtn = document.getElementById('inc-btn');
 const decBtn = document.getElementById('dec-btn');
 const resetBtn = document.getElementById('reset-btn');
+const loadBtn = document.getElementById('load-btn');
 const loaderLine = document.getElementById('loader-line');
 
 // 400 is 0
+let interval = 0;
+
+loadBtn.addEventListener('click', () => {
+  if (interval) {
+    return;
+  }
+  interval = setInterval(() => {
+    const currentProgress = loaderLine.style.strokeDasharray;
+    if (Number(currentProgress) === 0 || Number(currentProgress) >= 300) {
+      loaderLine.style.strokeDasharray = '200';
+      return;
+    }
+    if (Number(currentProgress) === 200) {
+      loaderLine.style.strokeDasharray = '300';
+    }
+  }, 500);
+});
 
 incBtn.addEventListener('click', () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = 0;
+    loaderLine.style.strokeDasharray = '426';
+    return;
+  }
   const currentProgress = loaderLine.style.strokeDasharray;
   if (Number(currentProgress) === 0) {
     loaderLine.style.strokeDasharray = '426';
@@ -17,6 +41,10 @@ incBtn.addEventListener('click', () => {
 });
 
 decBtn.addEventListener('click', () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = 0;
+  }
   const currentProgress = loaderLine.style.strokeDasharray;
   if (Number(currentProgress) > 400) {
     loaderLine.style.strokeDasharray = `${Number(currentProgress) - 26}`;
@@ -24,8 +52,9 @@ decBtn.addEventListener('click', () => {
 });
 
 resetBtn.addEventListener('click', () => {
-  const currentProgress = loaderLine.style.strokeDasharray;
-  if (Number(currentProgress) > 400) {
-    loaderLine.style.strokeDasharray = '400';
+  if (interval) {
+    clearInterval(interval);
+    interval = 0;
   }
+  loaderLine.style.strokeDasharray = '400';
 });
