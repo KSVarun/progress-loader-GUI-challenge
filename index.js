6f78542b46b6f5ae29a5cda7a82beddb51b6acd9
KSVarun <ksvarun001@gmail.com>
2022-03-17 22:57:25 +0530

    progress bar with path

--- index.js
+++ index.js
@@ -3,35 +3,29 @@ const decBtn = document.getElementById('dec-btn');
 const resetBtn = document.getElementById('reset-btn');
 const loaderLine = document.getElementById('loader-line');
 
-function getValueFromPercent(percent) {
-  return Number(percent.split('%')[0]);
-}
+// 400 is 0
 
 incBtn.addEventListener('click', () => {
-  const currentProgress = getValueFromPercent(loaderLine.getAttribute('x2'));
-  if (currentProgress < 98) {
-    loaderLine.style.strokeWidth = 8;
-    loaderLine.setAttribute('x1', '2%');
-    loaderLine.setAttribute('x2', `${currentProgress + 14}%`);
+  const currentProgress = loaderLine.style.strokeDasharray;
+  if (Number(currentProgress) === 0) {
+    loaderLine.style.strokeDasharray = '426';
+    return;
+  }
+  if (Number(currentProgress) < 790) {
+    loaderLine.style.strokeDasharray = `${Number(currentProgress) + 26}`;
   }
 });
 
 decBtn.addEventListener('click', () => {
-  const currentProgress = getValueFromPercent(loaderLine.getAttribute('x2'));
-  if (currentProgress > 0) {
-    loaderLine.setAttribute('x2', `${currentProgress - 14}%`);
-    if (currentProgress === 14) {
-      loaderLine.style.strokeWidth = 0;
-      loaderLine.setAttribute('x1', '0%');
-    }
+  const currentProgress = loaderLine.style.strokeDasharray;
+  if (Number(currentProgress) > 400) {
+    loaderLine.style.strokeDasharray = `${Number(currentProgress) - 26}`;
   }
 });
 
 resetBtn.addEventListener('click', () => {
-  const currentProgress = getValueFromPercent(loaderLine.getAttribute('x2'));
-  if (currentProgress > 0) {
-    loaderLine.setAttribute('x2', '0%');
-    loaderLine.setAttribute('x1', '0%');
-    loaderLine.style.strokeWidth = 0;
+  const currentProgress = loaderLine.style.strokeDasharray;
+  if (Number(currentProgress) > 400) {
+    loaderLine.style.strokeDasharray = '400';
   }
 });
