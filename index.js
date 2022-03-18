a4226f905d0142447adb975ca8bffb73918c0812
KSVarun <ksvarun001@gmail.com>
2022-03-18 11:03:34 +0530

    theming and animations

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const incBtn = document.getElementById('inc-btn');
 const decBtn = document.getElementById('dec-btn');
 const resetBtn = document.getElementById('reset-btn');
 const loadBtn = document.getElementById('load-btn');
+const completeBtn = document.getElementById('complete-btn');
 const loaderLine = document.getElementById('loader-line');
 
+function sleep(time) {
+  return new Promise((res, rej) => {
+    setTimeout(() => res(), time);
+  });
+}
+
 // 400 is 0
+const sleepTime = 100;
 
 loadBtn.addEventListener('click', () => {
+  loaderLine.style.strokeDasharray = '400';
   if (loaderLine.classList.contains('load')) {
     loaderLine.classList.remove('load');
     return;
@@ -14,9 +23,10 @@ loadBtn.addEventListener('click', () => {
   loaderLine.classList.add('load');
 });
 
-incBtn.addEventListener('click', () => {
+incBtn.addEventListener('click', async () => {
   if (loaderLine.classList.contains('load')) {
     loaderLine.classList.remove('load');
+    await sleep(sleepTime);
     loaderLine.style.strokeDasharray = '426';
     return;
   }
@@ -33,6 +43,7 @@ incBtn.addEventListener('click', () => {
 decBtn.addEventListener('click', () => {
   if (loaderLine.classList.contains('load')) {
     loaderLine.classList.remove('load');
+    return;
   }
   const currentProgress = loaderLine.style.strokeDasharray;
   if (Number(currentProgress) > 400) {
@@ -40,9 +51,18 @@ decBtn.addEventListener('click', () => {
   }
 });
 
-resetBtn.addEventListener('click', () => {
+completeBtn.addEventListener('click', async () => {
   if (loaderLine.classList.contains('load')) {
     loaderLine.classList.remove('load');
   }
+  await sleep(sleepTime);
+  loaderLine.style.strokeDasharray = '790';
+});
+
+resetBtn.addEventListener('click', async () => {
+  if (loaderLine.classList.contains('load')) {
+    loaderLine.classList.remove('load');
+  }
+  await sleep(sleepTime);
   loaderLine.style.strokeDasharray = '400';
 });
